03f803a256a4b95d48bb6e85b33c0ecdd4b2803f
Amirkian Kiani <amirkiankiani@icloud.com>
2023-08-18 08:14:42 +0330

    [modify] loginForm

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
@@ -6,51 +8,79 @@ class LoginForm extends Component {
       username: "",
       password: "",
     },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
 
+    const errors = this.validate();
+    // console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     // Call the server
     console.log("Submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+      // ...
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+      // ...
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              value={account.username}
-              onChange={this.handleChange}
-              id="username"
-              name="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            label="Username"
+            name="username"
+            value={account.username}
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            label="Password"
+            name="password"
+            value={account.password}
+            onChange={this.handleChange}
+            error={errors.password}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
