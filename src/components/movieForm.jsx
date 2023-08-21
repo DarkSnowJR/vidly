c2171492d4bc6949d00216e216460850165092e1
Amirkian Kiani <74903450+DarkSnowJR@users.noreply.github.com>
2023-08-21 12:16:36 +0330

    [modify] loginForm component

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -4,7 +4,7 @@ const MovieForm = ({ match, history }) => {
   return (
     <div>
       <h1> Movie Form {match.params.id}</h1>
-      <button class="btn btn-primary" onClick={() => history.push("/movies")}>
+      <button className="btn btn-primary" onClick={() => history.push("/movies")}>
         Save
       </button>
     </div>
